22ba08b067abaacfb48a37cc49524a99bfc06cac
gahette <gahette@icloud.com>
2022-04-09 20:10:44 +0200

    start filter

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -6,7 +6,16 @@ function Pictures() {
     const [pictures, setPictures] = useState([])
     const [isloading, setIsloading] = useState(true)
     const [cats, setCats] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState("");
     const [navbarOpen, setNavbarOpen] = useState(false)
+
+    //remove duplicates in array cats
+    const newCats = [...new Map(cats.map((item) => [item["name"], item])).values()]
+
+    const handleClick = (event) => {
+        setSelectedCategory(event.target.value)
+
+    }
 
     const handleNavbarOpen = () => {
         setNavbarOpen(!navbarOpen)
@@ -20,7 +29,7 @@ function Pictures() {
 
         axios.get(`${BACKEND_URL}/items/galery`, {signal: signal})
             .then(res => {
-
+                setCats(res.data.data)
                 setPictures(res.data.data)
                 setIsloading(false)
 
@@ -32,49 +41,41 @@ function Pictures() {
 
     }, [])
 
-    useEffect(() => {
+    return <div className='galery'>
 
-        const abortController = new AbortController();
-        const signal = abortController.signal;
+        <div className={`navbar ${navbarOpen ? 'show-nav' : 'hide-nav'}`}>
 
+            <ul className="navbar__links">
 
-        axios.get(`${BACKEND_URL}/items/categories`, {signal: signal})
-            .then(res => {
-                setCats(res.data.data)
-                setIsloading(false)
-            })
-
-        return function cleanup() {
-            abortController.abort()
-        }
-
-    }, [])
+                {isloading ? 'Loading..' :
+                    newCats
+                        .filter((newCat)=>newCat.name[0].includes(selectedCategory))
+                        .sort((a, b) => a.name.localeCompare(b.name))
+                        .map((newCat, index) =>
 
 
-    return (
-        <div className='galery'>
 
-            <div className={`navbar ${navbarOpen ? 'show-nav' : 'hide-nav'}`}>
+                    <li className="navbar__link" key={index}  >
 
-                <ul className="navbar__links">
-                    {isloading ? 'Loading..' : cats.map((cat, index) =>
-                        <li className="navbar__link" key={index}><span>{cat.name}</span></li>)}
-                </ul>
-                <button className='burger' onClick={handleNavbarOpen}>
-                    <span className="bar">
+                    <span onClick={handleClick}>{newCat.name}</span>
+                    </li>)
+                }
 
-                    </span>
-                </button>
-            </div>
-            <section className='images'>
-                {isloading ? 'Loading..' : pictures.map((picture, index) =>
+            </ul>
+            <button className='burger' onClick={handleNavbarOpen}>
+                <span className="bar">
+
+                </span>
+            </button>
+        </div>
+        <section className='images'>
+            {isloading ? 'Loading..' : pictures
+                .map((picture, index) =>
                     <img src={`${BACKEND_URL}/assets/` + picture.picture} key={index}
                          alt="photo de la page d'accueil"/>)}
-            </section>
+        </section>
 
-        </div>
-
-    );
+    </div>;
 }
 
 export default Pictures;
