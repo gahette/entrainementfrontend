1e6ae02ade857d10403ec5333f296568bdabbcd7
gahette <gahette@icloud.com>
2022-04-22 19:44:50 +0200

    buton radio start responsive galery

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -7,12 +7,14 @@ function Pictures() {
     const [cats, setCats] = useState([])
     const [selectedCategory, setSelectedCategory] = useState("");
     const [navbarOpen, setNavbarOpen] = useState(false)
+    const [selectedRadio, setSelectedRadio] = useState("");
+
 
     //remove duplicates in array cats
     const newCats = [...new Map(cats.map((item) => [item["categories"], item])).values()]
+
     const handleClick = (event) => {
         setSelectedCategory(event.target.id)
-
     }
 
     const handleNavbarOpen = () => {
@@ -43,30 +45,59 @@ function Pictures() {
     return (
         <div className='galery'>
 
-            <div className={`navbar ${navbarOpen ? 'show-nav' : 'hide-nav'}`}>
-                <ul className="navbar__links" onClick={handleNavbarOpen}>
+             <div className={`navbar ${navbarOpen ? 'show-nav' : 'hide-nav'}`}>
+                 <ul className="navbar__links" onClick={handleNavbarOpen}>
+                     {isloading ? 'Loading..' :
+                         newCats
+                             .sort((a, b) => a.categories.localeCompare(b.categories))
+                             .map((newCat, index) =>
+                                 <li className="navbar__link" key={index}>
+                                     <span id={newCat.categories.toLocaleString()}
+                                           onClick={handleClick}>{newCat.categories}</span>
+                                 </li>)}
+                 </ul>
+                 <button className='burger' onClick={handleNavbarOpen}>
+                 <span className="bar">
+                 </span>
+                 </button>
+
+
+             </div>
+            <div className='radios-galery'>
+
+                <ul className='radio-galery'>
                     {isloading ? 'Loading..' :
                         newCats
-                            .sort((a, b) => a.categories.localeCompare(b.categories))
-                            .map((newCat, index) =>
-                                <li className="navbar__link" key={index}>
-                                    <span id={newCat.categories.toLocaleString()} onClick={handleClick}>{newCat.categories}</span>
-                                </li>)}
+                            .map((newCat,index) => (
+                                <li key={index}>
+                                    <input
+                                        type="radio"
+                                        id={newCat.categories.toLocaleString()}
+                                        value={newCat.categories}
+                                        name="reCat"
+                                        checked={newCat.categories === selectedRadio}
+                                        onChange={(e) => setSelectedRadio(e.target.id)}
+                                    />
+                                    <label htmlFor={newCat}>{newCat.categories}</label>
+                                </li>
+                            ))}
                 </ul>
-                <button className='burger' onClick={handleNavbarOpen}>
-                <span className="bar">
-                </span>
-                </button>
+
+
             </div>
 
-            <div className="cancel">
+             <div className="cancel">
                 {selectedCategory &&
-                    <h5 className='button-cancel' onClick={() => setSelectedCategory("")}>Annuler recherche</h5>}
-            </div>
+                     <h5 className='button-cancel' onClick={() => setSelectedCategory("")}>Annuler recherche</h5>}
+                 {selectedRadio && (
+                     <h5 className='button-cancel' onClick={() => setSelectedRadio("")}>Annuler la recherche</h5>
+                 )}
+             </div>
 
             <section className='images'>
                 {isloading ? 'Loading..' : pictures
                     .filter((picture) => picture.categories.includes(selectedCategory))
+                    .filter((picture) => picture.categories.includes(selectedRadio))
                     .map((picture, index) =>
                         <img src={picture.mainImage.asset.url} key={index}
                              alt="photos galerie"/>)}
