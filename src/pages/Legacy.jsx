11801a1dd8f97b2690278abf359dea62e56f59d4
gahette <gahette@icloud.com>
2022-04-29 06:37:49 +0200

    fixes 5

--- src/pages/Legacy.jsx
+++ src/pages/Legacy.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from "../components/Header";
+
 
 function Legacy() {
     return (
         <div>
-            <Header/>
             Attention c'est une ARNAQUE !! Charles Cantin est un brouteur.....
         </div>
     );
